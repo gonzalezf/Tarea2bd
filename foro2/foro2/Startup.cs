5860dfbda1f5b095e00e969ff135e95df3c5eec3
gonzalezf <felipe.gonzalezp.12@sansano.usm.cl>
2014-05-20 21:41:03 -0400

    inicio de proyecto. falta codigo para guardar data de visual studio a bd. archivo de bd creado.

---
+++ foro2/foro2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(foro2.Startup))]
+namespace foro2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
