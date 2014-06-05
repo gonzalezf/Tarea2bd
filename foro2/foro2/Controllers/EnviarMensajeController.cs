5866ec16f3124e31818c164634aa1d748cf7015d
felipe <felipe.morales93@gmail.com>
2014-06-04 21:35:40 -0400

    xd

--- foro2/foro2/Controllers/EnviarMensajeController.cs
+++ foro2/foro2/Controllers/EnviarMensajeController.cs
@@ -16,12 +16,16 @@ namespace foro2.Controllers
         public ActionResult Index()
         {
             var m = new EnviarMensaje();
+            string[] error = {"", "", "", ""};
+            ViewBag.Error = error;
             return View(m);
         }
 
         [HttpPost] //este metodo recibe informacion, por eso va el httppost
         public ActionResult Index(EnviarMensaje m)
         {
+            string[] error = { "", "", "", "" };
+            ViewBag.Error = error;
             ViewBag.probandopara = m.para;
             ViewBag.probandoasunto = m.asunto;
             ViewBag.probandomensaje = m.mensaje;
@@ -32,7 +36,8 @@ namespace foro2.Controllers
             if(id_usuario == -1)
             {
                 //No existe el usuario espeficiado, morir!
-                ViewBag.Error = "No existe tal usuario ("+m.para+")!";
+                ViewBag.Error = error;
+                ViewBag.Error[0] = "No existe tal usuario ("+m.para+")!";
                 return View(m);
             }
 
@@ -41,27 +46,46 @@ namespace foro2.Controllers
             if(inbox_info.Count() <= 0)
             {
                 //Morir, no existe tal buzon!
-                 ViewBag.Error = "No encontre el buzon de ese usuario";
-                 return View(m);
+                ViewBag.Error = error;
+                ViewBag.Error[0] = "No encontre el buzon de ese usuario";
+                return View(m);
             }
             else if(int.Parse(inbox_info[0]) == -1)
             {
-                 ViewBag.Error = "No encontre el buzon de ese usuario";
+                ViewBag.Error = error;
+                 ViewBag.Error[0] = "No encontre el buzon de ese usuario";
                 //Morir, no existe tal buzon!
                  return View(m);
+            }
+            if(m.asunto.Length <= 0)
+            {
+                ViewBag.Error = error;
+                ViewBag.Error[1] = "No especificó un asunto";
+                return View(m);
+            }
+            if(m.mensaje.Length <= 0)
+            {
+                ViewBag.Error = error;
+                ViewBag.Error[2] = "El mensaje es muy corto";
+                return View(m);
+            }
+            else if(m.mensaje.Length > 350)
+            {
+                ViewBag.Error = error;
+                ViewBag.Error[2] = "El mensaje es muy largo";
+                return View(m);
             }
             int id_buzon = int.Parse(inbox_info[0]);
 
             string fecha_envio = DateTime.Now.ToString("dd/MM/yyyy");
             tf.Conectar();
+
             if(tf.EjecutarSql("INSERT INTO mensaje_privado VALUES('"+int.Parse((String) Session["UserId"])+"', '"+id_buzon.ToString()+"', 'False', '"+m.mensaje+"', '"+fecha_envio+"', '"+m.asunto+"')") <= 0)
             {
-                ViewBag.Error = "Error al ejecutar query!";
+                ViewBag.Error[0] = "Error al ejecutar query!";
+                return View(m);
             }
-            else
-            {
-                ViewBag.Error = "";
-            }
+            ViewBag.Error[3] = "¡Mensaje Enviado!";
             tf.Desconectar();
             return View(m);
         }
