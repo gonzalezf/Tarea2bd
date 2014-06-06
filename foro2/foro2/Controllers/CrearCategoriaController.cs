3f0230f8d5c2ed917b900d38ad2f2812d9764e31
gonzalezf <felipe.gonzalezp.12@sansano.usm.cl>
2014-06-06 01:12:19 -0400

    frf

---
+++ foro2/foro2/Controllers/CrearCategoriaController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Data.SqlClient;
+
+namespace foro2.Controllers
+{
+    public class CrearCategoriaController : Controller
+    {
+        //
+        // GET: /CrearCategoria/
+        public ActionResult Index()
+        {
+
+            if (String.Compare((String)Session["LoggedIn"], "Yes") != 0)
+            {
+                return Redirect("/");
+            }
+
+            if (String.Compare((String)Session["GroupId"], "1") != 0)
+            {
+                return Redirect("/");
+            }
+
+            ModeloCategoria categoria = new ModeloCategoria();
+
+          
+
+
+
+
+
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public ActionResult Index(ModeloCategoria categoria)
+        {
+
+            ViewBag.nombre = categoria.nombrecategoria;
+            ViewBag.descripcion = categoria.descripcioncategoria;
+            ViewBag.publico_categoria = categoria.publico_categoria;
+            
+        
+            tablasforo ManipularDatos = new tablasforo();
+            ManipularDatos.Conectar();
+
+
+            ManipularDatos.EjecutarSql("INSERT INTO categoria VALUES('" + categoria.nombrecategoria + "','" + categoria.descripcioncategoria+ "','" + categoria.publico_categoria + "')");
+            ManipularDatos.Desconectar();
+
+
+
+
+          //  return RedirectToAction("Index", new RouteValueDictionary(new { controller = "Inicio", action = "Index" }));
+            return View(categoria);
+        }
+
+	}
+}
---
+++ foro2/foro2/ModeloCategoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using foro2.Models;
+
+namespace foro2
+{
+    public class ModeloCategoria
+    {
+        public string nombrecategoria;
+        public string descripcioncategoria;
+        public bool publico_categoria;
+
+
+
+        public ModeloCategoria()
+        {
+            nombrecategoria = string.Empty;
+            descripcioncategoria = string.Empty;
+        }
+    }
+}
